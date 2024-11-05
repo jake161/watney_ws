628eb977bade79905ced0a5919b79443bc7b936d
Jacob Browning <62108812+jake161@users.noreply.github.com>
2024-11-05 16:02:16 -0500

    Quaternian Support

--- watney_control/src/main.cpp
+++ watney_control/src/main.cpp
@@ -9,6 +9,24 @@
 // Create the sensor object
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
+// Define a struct to hold the IMU data
+struct IMUData {
+  float orientation_w;
+  float orientation_x;
+  float orientation_y;
+  float orientation_z;
+  float accel_x;
+  float accel_y;
+  float accel_z;
+  float gyro_x;
+  float gyro_y;
+  float gyro_z;
+  float mag_x;
+  float mag_y;
+  float mag_z;
+  int8_t temp;
+};
+
 void setup() {
   // Start the serial communication
   Serial.begin(9600);
@@ -27,85 +45,80 @@ void setup() {
 }
 
 void loop() {
-  sensors_event_t event;
-  bno.getEvent(&event);
+  // Get quaternion orientation data
+  imu::Quaternion quat = bno.getQuat();
+  IMUData imu_data;
+
+  imu_data.orientation_w = quat.w();
+  imu_data.orientation_x = quat.x();
+  imu_data.orientation_y = quat.y();
+  imu_data.orientation_z = quat.z();
+
+  // Get acceleration data
+  imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  imu_data.accel_x = accel.x();
+  imu_data.accel_y = accel.y();
+  imu_data.accel_z = accel.z();
+
+  // Get gyroscope data
+  imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  imu_data.gyro_x = gyro.x();
+  imu_data.gyro_y = gyro.y();
+  imu_data.gyro_z = gyro.z();
+
+  // Get magnetometer data
+  imu::Vector<3> mag = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+  imu_data.mag_x = mag.x();
+  imu_data.mag_y = mag.y();
+  imu_data.mag_z = mag.z();
+
+  // Get temperature data
+  imu_data.temp = bno.getTemp();
 
 #ifdef DEBUG
   // Human-readable output
   Serial.print("Orientation: ");
-  Serial.print("X: ");
-  Serial.print(event.orientation.x);
+  Serial.print("W: ");
+  Serial.print(imu_data.orientation_w);
+  Serial.print(" X: ");
+  Serial.print(imu_data.orientation_x);
   Serial.print(" Y: ");
-  Serial.print(event.orientation.y);
+  Serial.print(imu_data.orientation_y);
   Serial.print(" Z: ");
-  Serial.println(event.orientation.z);
+  Serial.println(imu_data.orientation_z);
 
-  imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
   Serial.print("Accel: ");
   Serial.print("X: ");
-  Serial.print(accel.x());
+  Serial.print(imu_data.accel_x);
   Serial.print(" Y: ");
-  Serial.print(accel.y());
+  Serial.print(imu_data.accel_y);
   Serial.print(" Z: ");
-  Serial.println(accel.z());
+  Serial.println(imu_data.accel_z);
 
-  imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
   Serial.print("Gyro: ");
   Serial.print("X: ");
-  Serial.print(gyro.x());
+  Serial.print(imu_data.gyro_x);
   Serial.print(" Y: ");
-  Serial.print(gyro.y());
+  Serial.print(imu_data.gyro_y);
   Serial.print(" Z: ");
-  Serial.println(gyro.z());
+  Serial.println(imu_data.gyro_z);
 
-  imu::Vector<3> mag = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
   Serial.print("Mag: ");
   Serial.print("X: ");
-  Serial.print(mag.x());
+  Serial.print(imu_data.mag_x);
   Serial.print(" Y: ");
-  Serial.print(mag.y());
+  Serial.print(imu_data.mag_y);
   Serial.print(" Z: ");
-  Serial.println(mag.z());
+  Serial.println(imu_data.mag_z);
 
-  int8_t temp = bno.getTemp();
   Serial.print("Temperature: ");
-  Serial.print(temp);
+  Serial.print(imu_data.temp);
   Serial.println(" C");
 #else
-  // CSV format output
-  imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  imu::Vector<3> mag = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
-  int8_t temp = bno.getTemp();
-
-  Serial.print(event.orientation.x);
-  Serial.print(",");
-  Serial.print(event.orientation.y);
-  Serial.print(",");
-  Serial.print(event.orientation.z);
-  Serial.print(",");
-  Serial.print(accel.x());
-  Serial.print(",");
-  Serial.print(accel.y());
-  Serial.print(",");
-  Serial.print(accel.z());
-  Serial.print(",");
-  Serial.print(gyro.x());
-  Serial.print(",");
-  Serial.print(gyro.y());
-  Serial.print(",");
-  Serial.print(gyro.z());
-  Serial.print(",");
-  Serial.print(mag.x());
-  Serial.print(",");
-  Serial.print(mag.y());
-  Serial.print(",");
-  Serial.print(mag.z());
-  Serial.print(",");
-  Serial.print(temp);
-  Serial.println();
+  // Raw data output using struct
+  Serial.write((uint8_t*)&imu_data, sizeof(imu_data));
 #endif
 
   // Delay for a bit before reading again
-  delay(1000);
+  delay(200);
 }
