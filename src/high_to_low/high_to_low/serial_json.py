67197184aefa13b9270c5dfc374b2603c918fa18
jake161 <62108812+jake161@users.noreply.github.com>
2025-01-21 11:42:43 -0500

    Added JSON interp.

--- src/high_to_low/high_to_low/serial_json.py
+++ src/high_to_low/high_to_low/serial_json.py
@@ -4,12 +4,13 @@ from std_msgs.msg import String
 import serial
 import threading
 import argparse
+import json
 
 class SerialNode(Node):
     def __init__(self, port, baudrate):
         super().__init__('high_to_low_serial_node')
-        self.publisher_ = self.create_publisher(String, '/read', 10)
-        self.subscription = self.create_subscription(String, '/write', self.write_serial, 10)
+        self.publisher_ = self.create_publisher(String, '/h2l/read', 10) #continuosly publish to read
+        self.subscription = self.create_subscription(String, '/h2l/write', self.write_serial, 10) # setup callback to write_serial
         self.subscription  # prevent unused variable warning
         self.ser = serial.Serial(port, baudrate, dsrdtr=None)
         self.ser.setRTS(False)
@@ -23,9 +24,18 @@ class SerialNode(Node):
             data = self.ser.readline().decode('utf-8')
             if data:
                 self.get_logger().info(f"Received: {data}")
-                msg = String()
-                msg.data = data
-                self.publisher_.publish(msg)
+                self.handle_json(data)
+
+    def handle_json(self, data):
+        # parsing json for correct topic allocation
+        try:
+            json_data = json.loads(data)
+            self.get_logger().info(f"Parsed JSON: {json_data}")
+            msg = String()
+            msg.data = json.dumps(json_data)
+            self.publisher_.publish(msg)
+        except json.JSONDecodeError as e:
+            self.get_logger().error(f"Failed to decode JSON: {e}")
 
     def write_serial(self, msg):
         self.ser.write(msg.data.encode() + b'\n')
@@ -33,8 +43,8 @@ class SerialNode(Node):
 def main(args=None):
     rclpy.init(args=args)
     parser = argparse.ArgumentParser(description='Serial JSON Communication')
-    parser.add_argument('port', type=str, nargs='?', default='/dev/serial0', help='Serial port name (e.g., COM1 or /dev/ttyUSB0)')
-    parser.add_argument('baudrate', type=int, nargs='?', default=115200, help='Serial baudrate (e.g., 9600 or 115200)')
+    parser.add_argument('port', type=str, nargs='?', default='/dev/serial0', help='Serial port name (e.g., COM1 or /dev/ttyUSB0)') # /dev/serial0 for RPi 4 UART pins
+    parser.add_argument('baudrate', type=int, nargs='?', default=115200, help='Serial baudrate (e.g., 9600 or 115200)') # 115200 default baudrate for UGV 6x4
     args = parser.parse_args()
     serial_node = SerialNode(args.port, args.baudrate)
     rclpy.spin(serial_node)
